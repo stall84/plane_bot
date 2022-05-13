6c4f81faa283f1cd89fdeedd97a10edd9f4ea3ee
Michael Stallings <stall84@gmail.com>
2022-05-13 13:06:24 -0400

    Reconfigured console logs in lambda for testing

--- src/lambdas/main.ts
+++ src/lambdas/main.ts
@@ -6,9 +6,10 @@ import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
 
-    console.log('Help, from inside this function !', context);
+    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return {
-        statusCode: 201,
+        statusCode: 200,
         body: JSON.stringify({
             message: 'TESTING'
         })
