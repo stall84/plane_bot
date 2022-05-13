abc5b2c6c2134de5e135277e5ad31a3e1faa1386
Michael Stallings <stall84@gmail.com>
2022-05-13 01:04:18 -0400

    Test lambda created

--- src/lambdas/main.ts
+++ src/lambdas/main.ts
@@ -1,15 +1,17 @@
 import fetch from 'node-fetch';
 import * as AWS from 'aws-sdk';
-import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyHandlerV2 } from 'aws-lambda';
 import { Context } from 'aws-sdk/clients/autoscaling';
+import { Event } from 'aws-sdk/clients/s3';
 
 
-export const handler = (event: APIGatewayProxyEvent, context: Context): APIGatewayProxyHandler => {
+export const handler = async (event: Event, context: Context): Promise<any> => {
 
+    console.log('Help, from inside this function !', context);
     return {
-        statusCode: 201,
+        statusCode: '201',
         body: JSON.stringify({
-            message: 'Unknown Endpoint'
+            message: 'TESTING'
         })
     };
 }
