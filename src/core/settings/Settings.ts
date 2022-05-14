6e45dfc4ca4f20c4fc0c7e989231f80663e2fac8
Michael Stallings <stall84@gmail.com>
2022-05-14 10:11:30 -0400

    Testing a simple scrape with lambda. Added cheerio

---
+++ src/core/settings/Settings.ts
@@ -0,0 +1,8 @@
+
+class Settings {
+    // protected URL_LIST: string[];
+}
+
+export const s = [
+    'https://www.controller.com/listings/search?Category=6&ModelGroup=CHEROKEE&Manufacturer=PIPER',
+];
--- src/lambdas/main.ts
+++ src/lambdas/main.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import * as AWS from 'aws-sdk';
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import * as cheerio from 'cheerio';
 
 
 
@@ -8,10 +9,38 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
 
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'TESTING'
-        })
-    };
+
+    const content: any = [];
+    
+    try {
+        const response = await fetch('https://www.controller.com/listings/search?Category=6&ModelGroup=CHEROKEE&Manufacturer=PIPER');
+        const data = await response.text();
+        const $ = cheerio.load(data);
+        $('.listing-portion-title', data).each(() => {
+            const title = $(this).text();
+            content.push(title);
+        });
+        console.log(`Debugging data : `, data);
+    } catch (error) {
+        console.warn('Error occurred scraping : ', error);
+    }
+
+
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Successfull Scrape at ${Date.now().toLocaleString} - See content`,
+                content: content
+            })
+        };
+        
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: `Error returning scraped content: ${error}`
+            })
+        };
+    }
 }
